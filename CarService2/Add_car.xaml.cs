e5850a578105a7d3d8ce99806b9d826230988ef9
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2023-07-21 23:56:33 +0100

    Add project files.

---
+++ CarService2/Add_car.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarService2
+{
+    /// <summary>
+    /// Interaction logic for Add_car.xaml
+    /// </summary>
+    public partial class Add_car : Window
+    {
+        public Add_car()
+        {
+            InitializeComponent();
+        }
+
+        private void Find_byReg_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CarService2/Car_info.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarService2
+{
+    /// <summary>
+    /// Interaction logic for Car_info.xaml
+    /// </summary>
+    public partial class Car_info : Page
+    {
+        public Car_info()
+        {
+            InitializeComponent();
+        }
+    }
+}
