708a0d2522af01ca23343b665948193aebc8c4ef
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2023-11-07 19:40:42 +0000

    Update

--- CarService2/Key/ApiKey.cs
+++ CarService2/Key/ApiKey.cs
@@ -8,7 +8,7 @@ namespace CarService2.Key
 {
     public class ApiKey
     {
-        public string Key = "KxClq64nPc306JlOC6auj9sDVlE8cn9V63sQljDN";
+        public string Key = "xxXxx";
                              
     }
 }
